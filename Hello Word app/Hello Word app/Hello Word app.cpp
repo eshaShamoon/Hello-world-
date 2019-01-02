72b3109b9c844f78249debfe423a9e7bcd07ec93
eshaShamoon <46321510+eshaShamoon@users.noreply.github.com>
2019-01-02 17:55:42 +0300

    Add files via upload

---
+++ Hello Word app/Hello Word app/Hello Word app.cpp
@@ -0,0 +1,16 @@
+// Hello Word app.cpp : Defines the entry point for the console application.
+//this programe is just for testing.
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "hello word!"<<endl;
+
+	getchar();
+    return 0;
+}
+
